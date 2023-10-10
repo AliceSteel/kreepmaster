5ba9fdf807e501af54e7a935b966460a2c9ebdee
Olga Garkusha <olga.garkusha.uk@gmail.com>
2023-10-10 09:17:55 +0200

    footer changes

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -73,47 +73,6 @@
           >AliceLab</a
         >
       </div>
-      <!-- <div
-        class="w-full h-full flex flex-wrap-reverse gap-4 justify-end sm:justify-between items-center py-16"
-      >
-        <div class="opacity-50 text-base">
-          Powered by:<a
-            href="https://alicelab.vercel.app/"
-            target="_blank"
-            class="menu_link_white ml-6"
-            >AliceLab</a
-          >
-        </div>
-        <ul class="w-full sm:w-1/2 flex flex-wrap justify-end">
-          <li class="text-right">More ways to contact:</li>
-          <li>
-            <a
-              href="mailto:kreepomatic@gmail.com"
-              rel="nofollow, noreferrer"
-              class="menu_link_white ml-6"
-              >Email</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://www.facebook.com/kreepmaster"
-              target="_blank"
-              rel="nofollow, noreferrer"
-              class="menu_link_white ml-6"
-              >Facebook</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/peter_c_utah/"
-              target="_blank"
-              rel="nofollow, noreferrer"
-              class="menu_link_white ml-6"
-              >Instagram</a
-            >
-          </li>
-        </ul>
-      </div>-->
     </div>
   </footer>
 </template>
