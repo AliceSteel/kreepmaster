ea18b4d1b77e7504dbad17e45ba209b12e26ef8c
Olga Garkusha <olga.garkusha.uk@gmail.com>
2023-10-10 09:17:41 +0200

    fixed footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,79 @@
 <template>
   <footer>
-    <div class="container sm:w-3/4 mx-auto">
-      <div
+    <div class="container sm:w-3/4 mx-auto py-8 pl-[10%]">
+      <ul class="w-full flex justify-left items-center gap-4 py-4">
+        <li>
+          <a
+            href="mailto:kreepomatic@gmail.com"
+            rel="nofollow, noreferrer"
+            title="Email"
+            class="flex p-4 bg-gray-50 rounded-full hover:bg-lime-700"
+            ><svg xmlns="http://www.w3.org/2000/svg" height="0.625em" viewBox="0 0 512 512">
+              <!--! Font Awesome Free 6.4.2 by fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+              <path
+                d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
+              />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/kreepmaster"
+            target="_blank"
+            title="Facebook"
+            rel="nofollow, noreferrer"
+            class="flex px-4 py-3 bg-gray-50 rounded-full hover:bg-lime-700"
+            ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
+              <!--! Font Awesome Free 6.4.2 by fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+              <path
+                d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+              />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/peter_c_utah/"
+            target="_blank"
+            title="Instagram"
+            rel="nofollow, noreferrer"
+            class="flex p-3 bg-gray-50 rounded-full hover:bg-lime-700"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+              <!--! Font Awesome Free 6.4.2 by fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+              <path
+                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+              />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/user/kreepmaster"
+            target="_blank"
+            title="Youtube"
+            rel="nofollow, noreferrer"
+            class="flex py-3 px-2.5 bg-gray-50 rounded-full hover:bg-lime-700"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+              <!--! Font Awesome Free 6.4.2 by fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+              <path
+                d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
+
+      <div class="opacity-50 text-base py-8">
+        Powered by:<a
+          href="https://alicelab.vercel.app/"
+          target="_blank"
+          class="menu_link_white ml-6"
+          >AliceLab</a
+        >
+      </div>
+      <!-- <div
         class="w-full h-full flex flex-wrap-reverse gap-4 justify-end sm:justify-between items-center py-16"
       >
         <div class="opacity-50 text-base">
@@ -41,7 +113,7 @@
             >
           </li>
         </ul>
-      </div>
+      </div>-->
     </div>
   </footer>
 </template>
